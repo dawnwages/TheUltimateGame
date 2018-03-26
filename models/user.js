f0fb8db5c97c1ad96e8b037ca061aee664ade62e
stoversa <stoversa@gmail.com>
2018-03-25 20:41:14 -0400

    update

--- models/user.js
+++ models/user.js
@@ -28,5 +28,15 @@ module.exports = function(sequelize, DataTypes) {
   User.hook("beforeCreate", function(user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
+
+  User.associate = function (models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.hasMany(models.Character, {
+      onDelete: "cascade"
+    });
+  };
+
+
   return User;
 };
