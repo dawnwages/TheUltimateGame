7f6431e63c1d50192f3bb5851ebe2efe0cb374c6
Dawn Wages <dawn.wages@gmail.com>
2018-03-27 02:03:41 -0400

    Game, script, siteroot/play

--- public/js/phaser/main.js
+++ public/js/phaser/main.js
@@ -1,11 +1,29 @@
 PlayState = {};
 
 PlayState.preload = function () {
+    this.game.load.json('level:1', 'data/level01.json');
     this.game.load.image('background', 'images/background.png');
-}
+
+    this.game.load.image('ground', 'images/ground.png');
+    this.game.load.image('grass:8x1', 'images/grass_8x1.png');
+    this.game.load.image('grass:6x1', 'images/grass_6x1.png');
+    this.game.load.image('grass:4x1', 'images/grass_4x1.png');
+    this.game.load.image('grass:2x1', 'images/grass_2x1.png');
+    this.game.load.image('grass:1x1', 'images/grass_1x1.png');
+};
 
 PlayState.create = function () {
     this.game.add.image(0,0, 'background');
+    this._loadLevel(this.game.cache.getJSON('level:1'));
+};
+
+PlayState._loadLevel = function(data){
+    console.log(data);
+    data.platforms.forEach(this._spawnPlatform, this);
+};
+
+PlayState._spawnPlatform = function (platform) {
+    this.game.add.sprite(platform.x, platform.y, platform.image);
 }
 
 //establishes screen size and initiates game
