565ace8c733a2064368cbfbbdc2e1084d59479c1
stoversa <stoversa@gmail.com>
2018-03-27 17:55:31 -0400

    Manging user creation, etc.

--- public/js/members.js
+++ public/js/members.js
@@ -1,8 +1,54 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
-    console.log("This user is logged in with ID: " + data.id);
+    $("#user-id").attr("data", data.id);
+    var id = data.id;
+    $.get("/api/character/" + id)
+      .then(function (response) {
+        if (!response) {
+          console.log("No character");
+        }
+        else {
+          console.log(response)
+          response.forEach(v => {
+            console.log(v.char_name)
+            $("#current-characters").append($("<button>")
+              .attr("character_id", v.id)
+              .attr("class", "created-characters")
+              .text("Continue as " + v.char_name));
+          })
+        }
+        // If there's an error, log the error
+      }).catch(function (err) {
+        console.log(err);
+      });
+  });
+
+  $(".create-char").click(function (event) {
+    event.preventDefault();
+    var characterSelection = $('input[name=character-selection]:checked');
+    $.post("/api/character", {
+      char_name: $("#char_name").val().trim(),
+      hp: characterSelection.attr('hp'),
+      attack: characterSelection.attr('attack'),
+      coins: 0,
+      lvl_comp: 0,
+      UserId: $("#user-id").attr('data')
+    }).then(function (data) {
+      console.log(data)
+      var playerId = data.id;
+      $.post("/api/pet", {
+        pet_name: $("#pet_name").val().trim(),
+        CharacterId: playerId
+      }).then(function (data) {
+        console.log(data);
+      }).catch(function (err) {
+        console.log(err)
+      })
+    }).catch(function (err) {
+      console.log(err);
+    });
   });
 });
